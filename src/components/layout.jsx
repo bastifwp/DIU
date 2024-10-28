a4430ad6b7cc3c9167ad3698bf53dcca5f6b7741
Scarleth Bazaes <scarleth_bazaes@hotmail.com>
2024-10-28 17:14:36 -0300

    added nav-bar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+//Importamos paginas
 import HomePage from '../pages/home_page'
 import LightbulbPage from '../pages/lightbulb_page'
 
+//Importamos componentes
 import NavBar from '../components/nav_bar'
+import Noticias from '../components/noticias'
+
 
 const Layout = () => {
   return (
     <BrowserRouter>
       <div className='layout'>
-        <h1 className='layout__title'>Interfaz energética</h1>
         <NavBar />
         <div className='layout__page'>
           <Routes>
@@ -18,8 +21,15 @@ const Layout = () => {
             <Route path='/lightbulb' element={<LightbulbPage />} />
           </Routes>
         </div>
+        <p> Abajo de esto estoy creando las noticias</p>
+        <Noticias />
       </div>
+
+      <div style= {{height: 500}}> </div>
     </BrowserRouter>
+
+    
+
   )
 }
 
